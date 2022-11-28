838aa357a01b863707d31e930004eae3300c8dbb
ssssudan <kankgm0324@gmail.com>
2022-11-28 19:45:02 +0900

    <feat>: 모든 기능에 대해서 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
+import baseball.domain.Judgment;
+import baseball.domain.NumberGenerator;
+import baseball.domain.Referee;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> computer = numberGenerator.createRandomNumbers();
+
+        Referee referee = new Referee();
+        String result = "";
+        while (!result.equals("0 볼 3 스트라이크 ")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다 ! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요: ");
+        String input = sc.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for(String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
